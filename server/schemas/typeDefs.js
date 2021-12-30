f97de808637befdfb694803ba448a39284a4debb
bmalbright <brianalbright@utexas.edu>
2021-12-30 16:59:44 -0600

    added the models folder to the server folder. created City.js, index.js, and User.js files in the models folder. fixed a typo in typeDefs.

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,7 +1,7 @@
 const { gql } = require('apollo-server-express');
 const { typeDefs } = require('.');
 
-const tyoeDefs = gql`
+const typeDefs = gql`
 type User {
     _id: ID!
     username: String!
