a5aafd10caf63fcb2abb6848cd750a25e6e9bbdf
bmalbright <brianalbright@utexas.edu>
2021-12-29 18:11:09 -0600

    created the schemas folder and added the typeDefs, resolvers, and index.js files to the folder.

---
+++ server/schemas/typeDefs.js
@@ -0,0 +1,30 @@
+const { gql } = require('apollo-server-express');
+const { typeDefs } = require('.');
+
+const tyoeDefs = gql`
+type User {
+    _id: ID!
+    username: String!
+    email: String
+    cityCount: Int
+    savedCities: [City]
+}
+
+type City {
+    cityId: ID!
+    cityName: String!
+}
+
+type Query {
+    me: User
+}
+
+type Mutation {
+    login(email: String!, password: String!): Auth
+    addUser(username : String!, email: String!, password: String!): Auth
+    savedCity(cityData: CityInput!): User
+    removeCity(cityId: ID!): User
+}
+`;
+
+module.exports - typeDefs
