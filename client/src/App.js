caa8e930d751b964c4cb0f7b34114a14a943e1f1
bmalbright <brianalbright@utexas.edu>
2022-01-01 19:54:36 -0600

    worked on injex.js and app.js. looked at css files. Added utils folder and created queries.js file in that folder.

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,56 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import {
+  ApolloClient, 
+  InMemoryCache, 
+  ApolloProvider,
+  createHttpLink,
+} from '@apollo/client';
+import { setContext } from '@apollo/client/link/context';
+
+import SearchCities from './pages/SearchCities';
+import SavedCities from './pages/SavedCities';
+import Navbar from './components/Navbar';
+
+// construct our main GraphQL API endpoint
+const httpLink = createHttpLink({
+  uri: '/graphql',
+});
+
+// construct request middleware that will attach the JWT token to every request as an 'authorization' header
+const authLink = setContext((_, { headers }) => {
+  //get the authentication token from local storage if it exists
+  const token = localStorage.getItem('it_token');
+  //return the headers to the context so httpLink can read them
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : '',
+    },
+  };
+});
+
+const client = new ApolloClient({
+  //set up our client to execute the `authlink` middleware prior to making the request to our GraphQL API
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache(),
+});
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <ApolloProvider client={client}>
+      <Router>
+        <>
+        <Navbar />
+        <Switch>
+          <Route exact path="/" component={SearchCities} />
+          <Route exact path="/saved" component={SavedCities} />
+          <Route render={() => <h1 className="display-2">Wrong Page!</h1>} />
+        </Switch>
+        </>
+      </Router>
+    </ApolloProvider>
+  )
 }
 
 export default App;
