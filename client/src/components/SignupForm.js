6731c0bf48ccc154e11edae2848aa4abe8845a70
bmalbright <brianalbright@utexas.edu>
2022-01-04 18:12:38 -0600

    added the signupform.js to the components folder

--- client/src/components/SignupForm.js
+++ client/src/components/SignupForm.js
@@ -1,19 +1,24 @@
-// see SignupForm.js for comments
-
 import React, { useState, useEffect } from "react";
 import { Form, Button, Alert } from "react-bootstrap";
 
 import { useMutation } from "@apollo/client";
-import { LOGIN_USER } from "../utils/mutations";
+import { ADD_USER } from "../utils/mutations";
 
 import Auth from "../utils/auth";
 
-const LoginForm = () => {
-  const [userFormData, setUserFormDate] = useState({ email: "", password: "" });
+const SignupForm = () => {
+  // sets initial form state
+  const [userFormData, setUserFormData] = useState({
+    username: "",
+    email: "",
+    password: "",
+  });
+  // sets state for initial form validation
   const [validated] = useState(false);
+  // sets state for an alert
   const [showAlert, setShowAlert] = useState(false);
 
-  const [login, { error }] = useMutation(LOGIN_USER);
+  const [addUser, { error }] = useMutation(ADD_USER);
 
   useEffect(() => {
     if (error) {
@@ -25,31 +30,31 @@ const LoginForm = () => {
 
   const handleInputChange = (event) => {
     const { name, value } = event.target;
-    setUserFormatData({ ...userFormData, [name]: value });
+    setUserFormData({ ...userFormData, [name]: value });
   };
 
   const handleFormSubmit = async (event) => {
     event.preventDefault();
 
+    // checks if the form has everything ( see react-bootstrap docs )
     const form = event.currentTarget;
     if (form.checkValidity() === false) {
       event.preventDefault();
-      event.preventPropagation();
+      event.stopPropagation();
     }
 
     try {
-      const { data } = await login({
-        variables: { ...userFormData },
+      const { data } = await addUser({
+        variable: { ...userFormData },
       });
-
       console.log(data);
-      Auth.login(data.login.token);
-    } catch (e) {
-      console.error(e);
+      Auth.login(data.addUser.token);
+    } catch (err) {
+      console.error(err);
     }
 
-    // clears form values
     setUserFormData({
+      username: "",
       email: "",
       password: "",
     });
@@ -57,20 +62,38 @@ const LoginForm = () => {
 
   return (
     <>
+      {/* used to the validation functionality above */}
       <Form noValidate validated={validated} onSubmit={handleFormSubmit}>
+        {/* show alert if server response is bad */}
         <Alert
           dismissible
           onClose={() => setShowAlert(false)}
           show={showAlert}
           variant="danger"
         >
-          Something went wrong with your login credentials.
+          There was problem with your signup.
         </Alert>
+
+        <Form.Group>
+          <Form.Label htmlFor="username">Username</Form.Label>
+          <Form.Control
+            type="text"
+            placeholder="Enter a username."
+            name="username"
+            onChange={handleInputChange}
+            value={userFormData.username}
+            required
+          />
+          <Form.Control.Feedback type="invalid">
+            A username is required.
+          </Form.Control.Feedback>
+        </Form.Group>
+
         <Form.Group>
           <Form.Label htmlFor="email">Email</Form.Label>
           <Form.Control
-            type="text"
-            placeholder="Your Email Address"
+            type="email"
+            placeholder="Enter a valid email address"
             name="email"
             onChange={handleInputChange}
             value={userFormData.email}
@@ -85,18 +108,24 @@ const LoginForm = () => {
           <Form.Label htmlFor="password">Password</Form.Label>
           <Form.Control
             type="password"
-            placeholder="Your Password"
+            placeholder="Enter a secure password"
             name="password"
             onChange={handleInputChange}
             value={userFormData.password}
             required
           />
-          <Form.Control.Feedback>
-            A valid password is required.
+          <Form.Control.Feedback type="invalid">
+            A password is required.
           </Form.Control.Feedback>
         </Form.Group>
         <Button
-          disabled={!(userFormData.email && userFormData.password)}
+          disabled={
+            !(
+              userFormData.username &&
+              userFormData.email &&
+              userFormData.password
+            )
+          }
           type="submit"
           variant="success"
         >
@@ -107,4 +136,4 @@ const LoginForm = () => {
   );
 };
 
-export default LoginForm;
+export default SignupForm;
