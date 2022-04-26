cda873051cf66fc38aedb260b6e78dbccb205ebb
bmalbright <brianalbright@utexas.edu>
2022-04-25 20:10:38 -0500

    added in moment. added styling to card.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 
-import './App.css';
+// import './App.css';
 import React, {useEffect, useState } from 'react';
 import Weather from './components/Weather';
 
--- src/components/Weather.js
+++ src/components/Weather.js
@@ -1,19 +1,28 @@
 import React from 'react';
-// import './styles.css';
-import { Card } from 'semantic-ui-react';
+import './styles.css';
+// import { Card } from 'semantic-ui-react';
+import moment from 'moment'; 
 
 const CardExampleCard = ({weatherData}) => (
-<Card>
-    <Card.Content>
-        <Card.Header className='header'>{weatherData.name}</Card.Header>
-        <p>Temperature: {weatherData.main.temp} &deg;F</p>
-        <p>Sunrise: {new Date(weatherData.sys.sunrise * 1000).toLocaleTimeString('en-IN')}</p>
-        <p>Sunset: {new Date(weatherData.sys.sunset * 1000).toLocaleTimeString('en-IN')}</p>
-        <p>Humidity: {weatherData.main.humidity}</p>
-        <p>Windspeed: {weatherData.main.windspeed}</p>
-        <p>Description: {weatherData.weather[0].description}</p>
-    </Card.Content>
-</Card>
+<div className='main'>
+    <p className='header'>{weatherData.name}</p>
+        <div className='flex'>
+            <p className='day'>Day: {moment().format('dddd')}, <span>{moment().format('LL')}</span></p>
+            <p className='description'>Description: {weatherData.weather[0].description}</p>
+        </div>
+        <div className='flex'>
+        <p className='temp'>Temperature: {weatherData.main.temp} &deg;F</p>
+        <p className='temp'>Humidity: {weatherData.main.humidity}</p>
+        <p className='temp'>Windspeed: {weatherData.main.windspeed}</p>
+
+        </div>
+        <div className='flex'>
+        <p className='sunrise-sunset'>Sunrise: {new Date(weatherData.sys.sunrise * 1000).toLocaleTimeString('en-IN')}</p>
+        <p className='sunrise-sunset'>Sunset: {new Date(weatherData.sys.sunset * 1000).toLocaleTimeString('en-IN')}</p>
+        </div>
+
+
+</div>
 )
 
 export default CardExampleCard;
