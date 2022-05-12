2b5543f7b777f461aae3a214e68dd97d26a65668
bmalbright <brianalbright@utexas.edu>
2022-05-12 00:46:54 -0500

    working on netlify functions to hide api keys without adding a server side.

--- README.md
+++ README.md
@@ -3,6 +3,6 @@
 ### This is an app built from the React weather app course on FreeCodeCamp. The course was created by Nishant Kumar. 
 
 
-### I started a version of a React weather app in January, setting aside a limited amount of time for it before needing to work on another project. Then my dog, Coli, suddenly died of cancer. So I put this off for a bit. Now I've gone back and created the project from scratch using the Kumar's course. So I named the app after Coli. He and my other dog, Fiona, were my constant companions during my coding bootcamp, with Coli often interrupting my presentations to the class by barking at something that wasn't really a threat. He is missed greatly, especially when I'm coding. 
+### I started a version of a React weather app in January, setting aside a limited amount of time for it before needing to work on another project. Then my dog, Coli, suddenly died of cancer. So I put this off for a bit. Now I've gone back and created the project from scratch using Kumar's course. So I named the app after Coli. He and my other dog, Fiona, were my constant companions during my coding bootcamp, with Coli often interrupting my presentations to the class by barking at something that wasn't really a threat. He is missed greatly, especially when I'm coding. 
 
 ![RIP Coli](2021-01-01.jpg)
--- src/components/Weather.js
+++ src/components/Weather.js
@@ -1,28 +1,41 @@
-import React from 'react';
-import './styles.css';
-// import { Card } from 'semantic-ui-react';
-import moment from 'moment'; 
+import React from "react";
+import "./styles.css";
+import { Row, Card } from "react-bootstrap";
+import moment from "moment";
 
-const CardExampleCard = ({weatherData}) => (
-<div className='main'>
-    <p className='header'>{weatherData.name}</p>
-        <div className='flex'>
-            <p className='day'>{moment().format('dddd')}, <span>{moment().format('LL')}</span></p>
-            <p className='description'>Description: {weatherData.weather[0].description}</p>
-        </div>
-        <div className='flex'>
-        <p className='temp'>Temperature: {weatherData.main.temp} &deg;F</p>
-        <p className='temp'>Humidity: {weatherData.main.humidity}%</p>
-        <p className='temp'>Windspeed: {weatherData.wind.speed}</p>
-
-        </div>
-        <div className='flex'>
-        <p className='sunrise-sunset'>Sunrise: {new Date(weatherData.sys.sunrise * 1000).toLocaleTimeString('en-IN')}</p>
-        <p className='sunrise-sunset'>Sunset: {new Date(weatherData.sys.sunset * 1000).toLocaleTimeString('en-IN')}</p>
-        </div>
-
-
-</div>
-)
+const CardExampleCard = ({ weatherData }) => (
+  <div className="main">
+    <Card>
+      <p className="header">{weatherData.name}</p>
+      <div className="flex">
+        <Row>
+          <p className="day">
+            {moment().format("dddd")}, <span>{moment().format("LL")}</span>
+          </p>
+        {/* </Row>
+        <Row> */}
+          <p className="description">
+            Current conditions: {weatherData.weather[0].description}
+          </p>
+        </Row>
+      </div>
+      <div className="flex">
+        <p className="temp">Temperature: {weatherData.main.temp} &deg;F</p>
+        <p className="temp">Humidity: {weatherData.main.humidity}%</p>
+        <p className="temp">Windspeed: {weatherData.wind.speed}</p>
+      </div>
+      <div className="flex">
+        <p className="sunrise-sunset">
+          Sunrise:{" "}
+          {new Date(weatherData.sys.sunrise * 1000).toLocaleTimeString("en-IN")}
+        </p>
+        <p className="sunrise-sunset">
+          Sunset:{" "}
+          {new Date(weatherData.sys.sunset * 1000).toLocaleTimeString("en-IN")}
+        </p>
+      </div>
+    </Card>
+  </div>
+);
 
 export default CardExampleCard;
